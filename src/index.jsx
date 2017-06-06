4a7f4d87252247f7025ddacbdece0bc05cb9ea90
Satyam <satyam@satyam.com.ar>
2017-06-06 13:40:33 +0200

    Updated React to 15.5
    * Updated module dependencies, including React
    * `PropTypes` now come in its own module that needs to be imported
    separately
    * Added `package-lock.json` file as generated by npm5

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,8 @@
  *
  *
  */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class IdleMonitor extends Component {
 
